57b6d6fc63016c8f2f93319b5b2c12c648283a49
Sagar <sjjoshi27@gmail.com>
2019-03-23 13:38:10 +0000

    Added test file for maxElevation to the project.

---
+++ src/gpx-tests/maxElevation.cpp
@@ -0,0 +1,17 @@
+#include <boost/test/unit_test.hpp>
+
+#include "logs.h"
+#include "types.h"
+#include "route.h"
+
+using namespace GPS;
+
+
+
+
+
+BOOST_AUTO_TEST_SUITE( Route_maxElevation_N0774756 )
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
