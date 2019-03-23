002d736c9a94e0b5d16657ab1efd12d0e04e4f26
Sagar <sjjoshi27@gmail.com>
2019-03-23 15:00:50 +0000

    Added case ele_absent to test file.

--- src/gpx-tests/maxElevation.cpp
+++ src/gpx-tests/maxElevation.cpp
@@ -12,6 +12,10 @@ using namespace GPS;
 
 BOOST_AUTO_TEST_SUITE( Route_maxElevation_N0774756 )
 
-
+BOOST_AUTO_TEST_CASE( ele_absent )
+{
+   Route route = Route(LogFiles::GPXRoutesDir + "ele_absent.gpx", isFileName);
+   BOOST_CHECK_EQUAL( route.maxElevation(), NULL );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
