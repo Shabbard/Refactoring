77f470fc1dabc9eabeddea1a01d9b8900e7254b7
Matthew Ball <N0743797@my.ntu.ac.uk>
2019-03-23 23:14:57 +0000

    Branch, add design comment

--- src/gpx-tests/maxLongitudeN0743797.cpp
+++ src/gpx-tests/maxLongitudeN0743797.cpp
@@ -8,5 +8,15 @@ using namespace GPS;
 
 BOOST_AUTO_TEST_SUITE( Route_maxLongitude_N0743797 )
 
+const bool isFileName = true;
+
+/* The maxLongitude function is used to return the most easternly
+ * value for a given point. Hence to test this function, first
+ * the function should be tested for null values, then values beyond
+ * the limits of Longitude should be tested. Multiple points with
+ * the same maximum in the route should also be tested.
+ * Then real values can be tested to make sure the correct maximum
+ * longitude is returned.
+ */
 
 BOOST_AUTO_TEST_SUITE_END()
