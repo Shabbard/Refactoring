8884ff791adf722e1ebc885d517629be405b25a6
Matthew Robinson <matt@00n.me>
2019-03-26 11:34:30 +0000

    Added find_position_N0724629 test suite scaffold.

---
+++ src/gpx-tests/findPositionN0724629.cpp
@@ -0,0 +1,13 @@
+#include <boost/test/unit_test.hpp>
+
+#include "logs.h"
+#include "route.h"
+#include "track.h"
+
+using namespace GPS;
+
+BOOST_AUTO_TEST_SUITE( find_position_N0724629 )
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
