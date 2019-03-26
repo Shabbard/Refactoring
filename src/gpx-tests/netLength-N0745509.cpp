cd7b34fa202ef6fa9b127bb086f43892f1f293f3
Omar Lines <n0745509@my.ntu.ac.uk>
2019-03-26 12:00:08 +0000

    Added netLength test suite

---
+++ src/gpx-tests/netLength-N0745509.cpp
@@ -0,0 +1,15 @@
+#include <boost/test/unit_test.hpp>
+
+#include "logs.h"
+#include "types.h"
+#include "route.h"
+
+
+//N0745509
+
+using namespace GPS;
+
+BOOST_AUTO_TEST_SUITE( route_netLength )
+
+
+BOOST_AUTO_TEST_SUITE_END()
