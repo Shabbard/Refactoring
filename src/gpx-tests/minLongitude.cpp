1ccf89f8c35d16631d32ec2d29d03177d628ee32
George Hanlon <n0734498@my.ntu.ac.uk>
2019-03-26 12:38:04 +0000

    First test set up

--- src/gpx-tests/minLongitude.cpp
+++ src/gpx-tests/minLongitude.cpp
@@ -1,1 +1,19 @@
 #include <boost/test/unit_test.hpp>
+#include "logs.h"
+#include "types.h"
+#include "route.h"
+
+using namespace GPS;
+
+BOOST_AUTO_TEST_SUITE( Route_minLongitude )
+
+const bool isFileName = true;
+
+BOOST_AUTO_TEST_CASE( shortest_route )
+{
+   Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
+   BOOST_CHECK_EQUAL( route.minLongitude(), -0.898312 );
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
