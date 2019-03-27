e014d42c43b481f5f583e17ccd19072deef49b44
Matthew Ball <N0743797@my.ntu.ac.uk>
2019-03-27 16:06:35 +0000

    Update Test Cases, Add GPX Files For Unit Tests Include all test cases for maxLongitude and add the GPX files neccesary to test each unit test case.

--- src/gpx-tests/maxLongitudeN0743797.cpp
+++ src/gpx-tests/maxLongitudeN0743797.cpp
@@ -10,13 +10,33 @@ BOOST_AUTO_TEST_SUITE( Route_maxLongitude_N0743797 )
 
 const bool isFileName = true;
 
+// The longitude of the most Easterly point on the Route.
+
 /* The maxLongitude function is used to return the most easternly
- * value for a given point. Hence to test this function, first
- * the function should be tested for null values, then values beyond
- * the limits of Longitude should be tested. Multiple points with
- * the same maximum in the route should also be tested.
+ * value included in a given Route. 
+ * Multiple points with the same maximum in the route should also be tested.
  * Then real values can be tested to make sure the correct maximum
  * longitude is returned.
+ * And finally the function can be tested to see if the correct
+ * value is returned if no easterly values are present in the route. 
  */
+
+BOOST_AUTO_TEST_CASE( correctEasterlyValue )
+{
+   Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
+   BOOST_CHECK_EQUAL( route.maxLongitude(), 1.79662 );
+}
+
+BOOST_AUTO_TEST_CASE( sameMostEasterlyValues )
+{
+   Route route = Route(LogFiles::GPXRoutesDir + "sameMaximumEasterly.gpx", isFileName);
+   BOOST_CHECK_EQUAL( route.maxLongitude(), NULL );
+}
+
+BOOST_AUTO_TEST_CASE( noEasterlyValues )
+{
+   Route route = Route(LogFiles::GPXRoutesDir + "logitudeAbsent.gpx", isFileName);
+   BOOST_CHECK_EQUAL( route.maxLongitude(), NULL );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
