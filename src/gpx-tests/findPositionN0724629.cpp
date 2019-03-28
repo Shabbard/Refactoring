f62f71249d8df8cb8294e1c49dc51d166d217cfa
Matthew Robinson <matt@00n.me>
2019-03-28 13:17:07 +0000

    Added tests to check lat, long and ele of position can be found.

--- src/gpx-tests/findPositionN0724629.cpp
+++ src/gpx-tests/findPositionN0724629.cpp
@@ -8,6 +8,24 @@ using namespace GPS;
 
 BOOST_AUTO_TEST_SUITE( Route_Find_Position_N0724629 )
 
+const bool isFileName = true;
 
+BOOST_AUTO_TEST_CASE( CanGetLatitude )
+{
+   	Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
+   	BOOST_CHECK_EQUAL( route.findPosition("Q").latitude(), Position(-0.89982, -0.898312).latitude() );
+}
+
+BOOST_AUTO_TEST_CASE( CanGetLongitude )
+{
+   	Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
+   	BOOST_CHECK_EQUAL( route.findPosition("Q").longitude(), Position(-0.89982, -0.898312).longitude() );
+}
+
+BOOST_AUTO_TEST_CASE( CanGetElevation )
+{
+   	Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
+   	BOOST_CHECK_EQUAL( route.findPosition("Q").elevation(), Position(-0.89982, -0.898312, -20000).elevation() );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
