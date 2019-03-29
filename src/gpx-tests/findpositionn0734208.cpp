d2c9d7f710538a67b9bc96134ce819ce45bd6d99
N0734208 <n0734208@my.ntu.ac.uk>
2019-03-29 13:39:33 +0000

    Added more testing functionality, checking N/S and E/W

--- src/gpx-tests/findpositionn0734208.cpp
+++ src/gpx-tests/findpositionn0734208.cpp
@@ -15,16 +15,31 @@ BOOST_AUTO_TEST_SUITE(find_position_N0734208)
 
 const bool isFileName = true;
 
+
+
 BOOST_AUTO_TEST_CASE( findLatitude )
 {
     Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
     BOOST_CHECK_EQUAL( route.findPosition("Q").latitude(), Position(-0.89982, -0.898312).latitude() );
+
 }
 
-BOOST_AUTO_TEST_CASE( findLongitude )
+BOOST_AUTO_TEST_CASE( findLatitudePositive )
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
+    BOOST_CHECK_EQUAL( route.findPosition("E").latitude(), Position(1.79964, 1.79662).latitude() );
+}
+
+BOOST_AUTO_TEST_CASE( findLongitudeNegative )
 {
     Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
     BOOST_CHECK_EQUAL( route.findPosition("Q").longitude(), Position(-0.89982, -0.898312).longitude() );
+}
+
+BOOST_AUTO_TEST_CASE( findLongitudePositive )
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "ABQWE.gpx", isFileName);
+    BOOST_CHECK_EQUAL( route.findPosition("E").longitude(), Position(1.79964, 1.79662).longitude() );
 }
 
 BOOST_AUTO_TEST_CASE( findElevation )
