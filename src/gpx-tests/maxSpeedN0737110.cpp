0ca37de0e555d1461994119b2f44309747bf254a
Rob <n0737110@my.ntu.ac.uk>
2019-03-29 21:32:41 +0000

    Added base test file

---
+++ src/gpx-tests/maxSpeedN0737110.cpp
@@ -0,0 +1,18 @@
+#include <boost/test/unit_test.hpp>
+
+#include "logs.h"
+#include "route.h"
+#include "track.h"
+
+using namespace GPS;
+
+BOOST_AUTO_TEST_SUITE( Track_maxSpeed_N0737110 )
+
+
+BOOST_AUTO_TEST_CASE( )
+{
+ 
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
