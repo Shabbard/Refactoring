de6d178da973a32ac276f567ba2a7818a6fe8b84
N0734208 <n0734208@my.ntu.ac.uk>
2019-03-29 11:23:28 +0000

    Added Boost test cases for finding the latitude, longtitude and elevation as well as testing for out of range error

--- src/gpx-tests/findpositionn0734208.cpp
+++ src/gpx-tests/findpositionn0734208.cpp
@@ -3,12 +3,43 @@
 #include "logs.h"
 #include "route.h"
 #include "track.h"
+#include <iostream>
 
 //Created by Imogen Wilkinson - N0734208
+// Find the route point bearing the specified name.
+// Throws a std::out_of_range exception if the name is not found.
 
 using namespace GPS;
 
 BOOST_AUTO_TEST_SUITE(find_position_N0734208)
+
+const bool isFileName = true;
+
+BOOST_AUTO_TEST_CASE( findLatitude )
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
+    BOOST_CHECK_EQUAL( route.findPosition("Q").latitude(), Position(-0.89982, -0.898312).latitude() );
+}
+
+BOOST_AUTO_TEST_CASE( findLongitude )
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
+    BOOST_CHECK_EQUAL( route.findPosition("Q").longitude(), Position(-0.89982, -0.898312).longitude() );
+}
+
+BOOST_AUTO_TEST_CASE( findElevation )
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
+    BOOST_CHECK_EQUAL( route.findPosition("Q").elevation(), Position(-0.89982, -0.898312, -20000).elevation() );
+}
+
+BOOST_AUTO_TEST_CASE(nonExistantPosition)
+{
+    Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
+    BOOST_CHECK_THROW( route.findPosition("B"), std::out_of_range);
+}
+
+
 
 
 
