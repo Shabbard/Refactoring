073fd12988237dbb17f27172ae64f15ad770dd8c
N0734208 <n0734208@my.ntu.ac.uk>
2019-03-29 10:54:59 +0000

    Added find position testing cpp file (n0734208)

--- src/gpx-tests/findpositionn0734208.cpp
+++ src/gpx-tests/findpositionn0734208.cpp
@@ -1,1 +1,15 @@
+#include <boost/test/unit_test.hpp>
 
+#include "logs.h"
+#include "route.h"
+#include "track.h"
+
+//Created by Imogen Wilkinson - N0734208
+
+using namespace GPS;
+
+BOOST_AUTO_TEST_SUITE(find_position_N0734208)
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
