b9055c126b28daaf7963c0b76b318f699251de7b
Matthew Robinson <matt@00n.me>
2019-03-30 17:02:09 +0000

    Abstracted log file generation to createLogFile function.

--- src/gpx-tests/findPositionN0724629.cpp
+++ src/gpx-tests/findPositionN0724629.cpp
@@ -12,6 +12,22 @@
 
 using namespace GPS;
 
+
+std::string createLogFile(std::string name, GridWorldRoute routeLog)
+{
+	// Converts the GridWorldRoute in to GPX format.
+    std::string routeGPX = routeLog.toGPX(true, name);
+
+    std::string fileName = name + "-N0724629.gpx";
+
+    std::ofstream openedFile(LogFiles::GPXRoutesDir + name + "-N0724629.gpx");
+    openedFile << routeGPX;
+    openedFile.close();
+
+    return name + "-N0724629.gpx";
+}
+
+
 BOOST_AUTO_TEST_SUITE( Route_Find_Position_N0724629 )
 
 const bool isFileName = true;
@@ -25,16 +41,7 @@ BOOST_AUTO_TEST_CASE( CanGetPositiveLatitude )
    	// Generate a GPX log file for the with GridWorld constructor for CityCampus.
     GridWorldRoute routeLog = GridWorldRoute("KQLD", GridWorld(Earth::CityCampus, 0, 1000));
 
-    // Converts the GridWorldRoute in to GPX format.
-    std::string routeGPX = routeLog.toGPX(true, "CanGetPositiveLatitude");
-
-    std::string fileName = "CanGetPositiveLatitude-N0724629.gpx";
-
-    std::ofstream openedFile(LogFiles::GPXRoutesDir + fileName);
-    openedFile << routeGPX;
-    openedFile.close();
-
-    Route route = Route(LogFiles::GPXRoutesDir + fileName, isFileName);
+    Route route = Route(LogFiles::GPXRoutesDir + createLogFile("CanGetPositiveLatitude", routeLog), isFileName);
    	BOOST_CHECK_EQUAL( route.findPosition("K").latitude(), 52.9581 );
 }
 
@@ -47,16 +54,7 @@ BOOST_AUTO_TEST_CASE( CanGetPositiveLongitude )
    	// Generate a GPX log file for the with GridWorld constructor for Pontianak.
     GridWorldRoute routeLog = GridWorldRoute("KQLD", GridWorld(Earth::Pontianak, 0, 1000));
 
-    // Converts the GridWorldRoute in to GPX format.
-    std::string routeGPX = routeLog.toGPX(true, "CanGetLongitudeLatitude");
-
-    std::string fileName = "CanGetPositiveLongitude-N0724629.gpx";
-
-    std::ofstream openedFile(LogFiles::GPXRoutesDir + fileName);
-    openedFile << routeGPX;
-    openedFile.close();
-
-    Route route = Route(LogFiles::GPXRoutesDir + fileName, isFileName);
+    Route route = Route(LogFiles::GPXRoutesDir + createLogFile("CanGetPositiveLongitude", routeLog), isFileName);
    	BOOST_CHECK_EQUAL( route.findPosition("K").longitude(), 109.322 );
 }
 
@@ -69,16 +67,7 @@ BOOST_AUTO_TEST_CASE( CanGetPositiveElevation )
    	// Generate a GPX log file for the with GridWorld constructor for CliftonCampus.
     GridWorldRoute routeLog = GridWorldRoute("IJOF", GridWorld(Earth::CliftonCampus, 0, 0));
 
-    // Converts the GridWorldRoute in to GPX format.
-    std::string routeGPX = routeLog.toGPX(true, "CanGetPositiveElevation");
-
-    std::string fileName = "CanGetPositiveElevation-N0724629.gpx";
-
-    std::ofstream openedFile(LogFiles::GPXRoutesDir + fileName);
-    openedFile << routeGPX;
-    openedFile.close();
-
-    Route route = Route(LogFiles::GPXRoutesDir + fileName, isFileName);
+    Route route = Route(LogFiles::GPXRoutesDir + createLogFile("CanGetPositiveElevation", routeLog), isFileName);
    	BOOST_CHECK_EQUAL( route.findPosition("I").elevation(), 58 );
 }
 
@@ -121,16 +110,7 @@ BOOST_AUTO_TEST_CASE ( CanGetZeroLatitude )
 	// Generate a GPX log file for the with GridWorld constructor for EquatorialMeridian.
     GridWorldRoute routeLog = GridWorldRoute("IEKS", GridWorld(Earth::EquatorialMeridian, 0, 1000));
 
-    // Converts the GridWorldRoute in to GPX format.
-    std::string routeGPX = routeLog.toGPX(true, "CanGetZeroLatitude");
-
-    std::string fileName = "CanGetZeroLatitude-N0724629.gpx";
-
-    std::ofstream openedFile(LogFiles::GPXRoutesDir + fileName);
-    openedFile << routeGPX;
-    openedFile.close();
-
-    Route route = Route(LogFiles::GPXRoutesDir + fileName, isFileName);
+    Route route = Route(LogFiles::GPXRoutesDir + createLogFile("CanGetZeroLatitude", routeLog), isFileName);
    	BOOST_CHECK_EQUAL( route.findPosition("I").latitude(), 0 );
 }
 
@@ -143,16 +123,7 @@ BOOST_AUTO_TEST_CASE ( CanGetZeroLongitude )
 	// Generate a GPX log file for the with GridWorld constructor for NorthPole.
     GridWorldRoute routeLog = GridWorldRoute("FIWA", GridWorld(Earth::NorthPole, 0, 1000));
 
-    // Converts the GridWorldRoute in to GPX format
-    std::string routeGPX = routeLog.toGPX(true, "CanGetZeroLongitude");
-
-    std::string fileName = "CanGetZeroLongitude-N0724629.gpx";
-
-    std::ofstream openedFile(LogFiles::GPXRoutesDir + fileName);
-    openedFile << routeGPX;
-    openedFile.close();
-
-    Route route = Route(LogFiles::GPXRoutesDir + fileName, isFileName);
+    Route route = Route(LogFiles::GPXRoutesDir + createLogFile("CanGetZeroLongitude", routeLog), isFileName);
    	BOOST_CHECK_EQUAL( route.findPosition("F").longitude(), 0 );
 }
 
@@ -165,16 +136,7 @@ BOOST_AUTO_TEST_CASE ( CanGetZeroElevation )
 	// Generate a GPX log file for the with GridWorld constructor for EquatorialMeridian.
     GridWorldRoute routeLog = GridWorldRoute("LQHD", GridWorld(Earth::EquatorialMeridian, 0, 0));
 
-    // Converts the GridWorldRoute in to GPX format
-    std::string routeGPX = routeLog.toGPX(true, "CanGetZeroElevation");
-
-    std::string fileName = "CanGetZeroElevation-N0724629.gpx";
-
-    std::ofstream openedFile(LogFiles::GPXRoutesDir + fileName);
-    openedFile << routeGPX;
-    openedFile.close();
-
-    Route route = Route(LogFiles::GPXRoutesDir + fileName, isFileName);
+    Route route = Route(LogFiles::GPXRoutesDir + createLogFile("CanGetZeroElevation", routeLog), isFileName);
    	BOOST_CHECK_EQUAL( route.findPosition("L").elevation(), 0 );
 }
 
@@ -187,16 +149,7 @@ BOOST_AUTO_TEST_CASE ( ThrowsOutOfRangeIfNameNotFound )
 	// Generate a GPX log file for the with default GridWorld constructor
     GridWorldRoute routeLog = GridWorldRoute("FIWA", GridWorld());
 
-    // Converts the GridWorldRoute in to GPX format
-    std::string routeGPX = routeLog.toGPX(true, "ThrowsOutOfRangeIfNameNotFound");
-
-    std::string fileName = "ThrowsOutOfRangeIfNameNotFound-N0724629.gpx";
-
-    std::ofstream openedFile(LogFiles::GPXRoutesDir + fileName);
-    openedFile << routeGPX;
-    openedFile.close();
-
-    Route route = Route(LogFiles::GPXRoutesDir + fileName, isFileName);
+    Route route = Route(LogFiles::GPXRoutesDir + createLogFile("ThrowsOutOfRangeIfNameNotFound", routeLog), isFileName);
    	BOOST_CHECK_THROW( route.findPosition("K").elevation(), std::out_of_range );
 }
 
@@ -209,16 +162,7 @@ BOOST_AUTO_TEST_CASE ( ThrowsOutOfRangeIfElevationNotFound )
 	// Generate a GPX log file for the with GridWorld constructor for NorthPole.
     GridWorldRoute routeLog = GridWorldRoute(GridWorldRoute("EAL", GridWorld(Earth::NorthPole, 0, 1000)));
 
-    // Converts the GridWorldRoute in to GPX format
-    std::string routeGPX = routeLog.toGPX(true, "ThrowsOutOfRangeIfElevationNotFound");
-
-    std::string fileName = "ThrowsOutOfRangeIfElevationNotFound-N0724629.gpx";
-
-    std::ofstream openedFile(LogFiles::GPXRoutesDir + fileName);
-    openedFile << routeGPX;
-    openedFile.close();
-
-    Route route = Route(LogFiles::GPXRoutesDir + fileName, isFileName);
+    Route route = Route(LogFiles::GPXRoutesDir + createLogFile("ThrowsOutOfRangeIfElevationNotFound", routeLog), isFileName);
    	BOOST_CHECK_THROW( route.findPosition("A").elevation(), std::out_of_range );
 }
 
