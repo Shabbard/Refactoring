6e88462d88894e77377bb962bc40d0488ff17b41
Matthew Robinson <matt@00n.me>
2019-03-30 12:53:37 +0000

    Added test case comment blocks.

--- src/gpx-tests/findPositionN0724629.cpp
+++ src/gpx-tests/findPositionN0724629.cpp
@@ -14,24 +14,40 @@ BOOST_AUTO_TEST_SUITE( Route_Find_Position_N0724629 )
 
 const bool isFileName = true;
 
+/**
+* Test case: CanGetLatitude
+* Use:       Checks that it is possible to obtain a value for latitude in a GPX log file.
+*/
 BOOST_AUTO_TEST_CASE( CanGetLatitude )
 {
    	Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
    	BOOST_CHECK_EQUAL( route.findPosition("Q").latitude(), Position(-0.89982, -0.898312).latitude() );
 }
 
+/**
+* Test case: CanGetLongitude
+* Use:       Checks that it is possible to obtain a value for longitude in a GPX log file.
+*/
 BOOST_AUTO_TEST_CASE( CanGetLongitude )
 {
    	Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
    	BOOST_CHECK_EQUAL( route.findPosition("Q").longitude(), Position(-0.89982, -0.898312).longitude() );
 }
 
+/**
+* Test case: CanGetElevation
+* Use:       Checks that it is possible to obtain a value for elevation in a GPX log file.
+*/
 BOOST_AUTO_TEST_CASE( CanGetElevation )
 {
    	Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
    	BOOST_CHECK_EQUAL( route.findPosition("Q").elevation(), Position(-0.89982, -0.898312, -20000).elevation() );
 }
 
+/**
+* Test case: ThrowsOutOfRangeIfNameNotFound
+* Use:       Checks that the std::out_of_range exception is thrown if the specified name is not found.
+*/
 BOOST_AUTO_TEST_CASE ( ThrowsOutOfRangeIfNameNotFound )
 {
 	// Generate a GPX log file for the with default GridWorld constructor
