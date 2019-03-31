5377e7929399b466c7ca95f86af881a065eca210
Callum Axon <callumaxon@gmail.com>
2019-03-31 10:45:37 +0100

    Test for index at an integer overflow

--- src/gpx-tests/accessOperator.cpp
+++ src/gpx-tests/accessOperator.cpp
@@ -1,59 +1,58 @@
 #include <boost/test/unit_test.hpp>
 
+#include <limits> 
+
 #include "logs.h"
 #include "position.h"
 #include "route.h"
-#include "gridworld.h"
-#include "gridworld_track.h"
 #include "gridworld_route.h"
+
+#include "accessOperatorLogGenerator.h"
 
 using namespace GPS;
 
 BOOST_AUTO_TEST_SUITE(Route_accessOperator)
 
-const bool isFileName = true;
-
-GridWorld gridWorld;
-
-GridWorldRoute generateRoute(const GridWorldTrack & track)
-{
-    return GridWorldRoute(track.toString(), gridWorld);
-}
+const std::string singlePointFilename = "A.gpx";
 
 // Tests accessing a position on the route under normal circumstances.
 BOOST_AUTO_TEST_CASE(ItCanAccessAPositionAtAnIndex)
 {
-    GridWorldTrack track = GridWorldTrack("ABCD", 10, 0, gridWorld);
-    GridWorldRoute worldRoute = GridWorldRoute(track.toString(), gridWorld);
-    Route route = Route(worldRoute.toGPX(), false);
-    BOOST_CHECK_EQUAL(route[0].toString(), gridWorld['A'].toString());
+    GridWorldRoute generatedRoute = GPX::singlePositionLog(singlePointFilename);
+    Route route = Route(LogFiles::GPXRoutesDir + singlePointFilename, true);
+
+    BOOST_CHECK_NO_THROW(route[0]);
 }
 
 // Ensures that the appropriate exception is thrown when trying to 
 // access a position at an index greater than the number of positions
 BOOST_AUTO_TEST_CASE(ItThrowsAnExceptionWhenTryingToAccessOutOfRange)
 {
-    GridWorldTrack track = GridWorldTrack("A", 10, 0, gridWorld);
-    Route route = Route(generateRoute(track).toGPX(), false);
+    GridWorldRoute generatedRoute = GPX::singlePositionLog(singlePointFilename);
+    Route route = Route(LogFiles::GPXRoutesDir + singlePointFilename, true);
 
     BOOST_CHECK_THROW(route[1], std::out_of_range);
 }
 
-BOOST_AUTO_TEST_CASE(ItAccessRepeatedPointsCorrectly)
-{
-    GridWorldTrack track = GridWorldTrack("MHMR", 10, 0, gridWorld);
-    Route route = Route(generateRoute(track).toGPX(), false);
-
-    BOOST_CHECK_EQUAL(route[0].toString(), gridWorld['M'].toString());
-    BOOST_CHECK_EQUAL(route[2].toString(), gridWorld['M'].toString());
-}
-
+// -1 will get implicitly converted to a large version of an unsigned integer
+// which will be a large value of an unsigned int.
 BOOST_AUTO_TEST_CASE(ItDoesntAcceptANegativeIndex)
 {
-    GridWorldTrack track = GridWorldTrack("A", 10, 0, gridWorld);;
-    Route route = Route(generateRoute(track).toGPX(), false);
+    GridWorldRoute generatedRoute = GPX::singlePositionLog(singlePointFilename);
+    Route route = Route(LogFiles::GPXRoutesDir + singlePointFilename, true);
+
     BOOST_CHECK_THROW(route[-1], std::out_of_range);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+// this test will change depending on the implementation present on the
+// machine running the executable.
+BOOST_AUTO_TEST_CASE(ItHandlesAnIntegerOverflowWithAnOutOfRange)
+{
+    const unsigned int maximumUIntIndex = std::numeric_limits<unsigned int>::max();
+    GridWorldRoute generatedRoute = GPX::singlePositionLog(singlePointFilename);
+    Route route = Route(LogFiles::GPXRoutesDir + singlePointFilename, true);
 
+    BOOST_CHECK_THROW(route[maximumUIntIndex], std::out_of_range);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
