9d22ad86a2538ca86b68722d91bf6e1fd1042709
Leon <n0742908@my.ntu.ac.uk>
2019-04-01 15:28:42 +0100

    Implemented first test

--- src/gpx-tests/steepestGradientN0742908.cpp
+++ src/gpx-tests/steepestGradientN0742908.cpp
@@ -6,11 +6,28 @@
 
 using namespace GPS;
 
+// function to generate a log for each file when implemented..
+std::string generateLogs(std::string nameOfTest, GridWorldRoute route)
+{
+    std::string fileName = nameOfTest + ".gpx";
+    std::ofstream fileOutput(GPS::LogFiles::GPXRoutesDir + fileName);
+
+    fileOutput << route.toGPX(true, nameOfTest);
+    fileOutput.close();
+
+    return fileName;
+}
+
+
+/*
+*/
 BOOST_AUTO_TEST_SUITE( Steepest_Gradient_N0742908 )
 
-BOOST_AUTO_TEST_CASE(  )
+BOOST_AUTO_TEST_CASE( is_Negative_Gradient )
 {
-    
+    GridWorldRoute routeLog = GridWorldRoute("A");
+    Route route = Route(LogFiles::GPXRoutesDir + generateLogs("is_Negative_Gradient", routeLog), isFileName);
+    BOOST_CHECK_EQUAL( route.steepestGradient(),  -50);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
