af491b261ea84737f7743f0b0069ecd9b011bdfc
Ben Jackson <n0736563@my.ntu.ac.uk>
2019-04-02 12:49:50 +0100

    Implemented simple test

--- src/gpx-tests/restingTimeN0736563.cpp
+++ src/gpx-tests/restingTimeN0736563.cpp
@@ -2,8 +2,12 @@
 #include "logs.h"
 #include "track.h"
 
+#include <iostream>
 
-BOOST_AUTO_TEST_CASE(test)
+BOOST_AUTO_TEST_CASE(noRestingTime)
 {
-    GPS::Track bigTrack(/*GPS::LogFiles::GPXRoutesDir + */GPS::LogFiles::GPXTracksDir + "A1B3C.gpx", true);
+    GPS::Track track(/*GPS::LogFiles::GPXRoutesDir + */GPS::LogFiles::GPXTracksDir + "A1B3C.gpx", true);
+
+    BOOST_CHECK_EQUAL(track.restingTime(), 0);
 }
+
