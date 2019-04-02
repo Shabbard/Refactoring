ba9530d2e4bb7fc80e540ddb95991aaa15934c62
Matthew Ball <N0743797@my.ntu.ac.uk>
2019-04-02 15:26:23 +0100

    Redesign test suite, add new GPX files Redesign the test suite to now check for negatively signed numbers and added the GPX files for these tests.

--- src/gpx-tests/maxLongitudeN0743797.cpp
+++ src/gpx-tests/maxLongitudeN0743797.cpp
@@ -29,7 +29,11 @@ const bool isFileName = true;
  *
  * The function should also make sure it accounts for signed numbers,
  * and so will be tested with negative numbers that use a high value
- * compared to postive ints with smaller values.
+ * compared to postive ints with smaller values. Also signed purely negative
+ * numbers should be tested too.
+ *
+ * Signed numbers at the edge case of 0 should also be tested
+ * -0 and 0.
  *
  */
 
@@ -39,10 +43,17 @@ BOOST_AUTO_TEST_CASE( correctEasterlyValue )
    BOOST_CHECK_EQUAL( route.maxLongitude(), 1.79662 );
 }
 
-BOOST_AUTO_TEST_CASE( signedEasterlyValues )
+BOOST_AUTO_TEST_CASE( signedMixedEasterlyValues )
 {
    Route route = Route(LogFiles::GPXRoutesDir + "longitudeSigned-N0743797.gpx", isFileName);
    BOOST_CHECK_EQUAL( route.maxLongitude(), 1.12345);
 }
+
+BOOST_AUTO_TEST_CASE( signedNegativeEasterlyValues )
+{
+   Route route = Route(LogFiles::GPXRoutesDir + "longitudeNegative-N0743797.gpx", isFileName);
+   BOOST_CHECK_EQUAL( route.maxLongitude(), 1.12345);
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
