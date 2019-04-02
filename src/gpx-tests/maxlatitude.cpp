360db65995333296d1afd0b40257ccef575a7e65
N0731234 <N0731234@my.ntu.ac.uk>
2019-04-02 12:40:48 +0100

    adding auto xml generation

--- src/gpx-tests/maxlatitude.cpp
+++ src/gpx-tests/maxlatitude.cpp
@@ -1,44 +1,52 @@
 #include <boost/test/unit_test.hpp>
-#include "../../headers/position.h"
+#include "position.h"
 #include "../../headers/route.h"
-#include "../../headers/logs.h"
+#include "logs.h"
+#include "../../headers/gridworld_route.h"
+#include <fstream>
 using namespace GPS;
 
 // Tests created by James bray, N0731234
 
+// create gpx log files
+void createGPXlog(GridWorldRoute gwr, std::string fileName){
+    fileName+="-N0731234.gpx";
+    // takes a grid world and returns string points
+    std::string route = gwr.toGPX(true, fileName);
+
+    // make file
+    std::ofstream file;
+    file.open(fileName); // create file with name
+    file << route; // push route points into file
+    file.close(); // close file
+}
 
 BOOST_AUTO_TEST_SUITE(Route_maxLatitude)
 
-BOOST_AUTO_TEST_CASE(expected_max)
-{
-    // test with just one possible outcome
-    Route route1 = Route(LogFiles::logsDir+"Q.gpx", true);
-    BOOST_CHECK_EQUAL(route1.maxLatitude(),Position(-0.89982, -0.898312));
-
-    // test with multiple inputs
-    Route route2 = Route(LogFiles::logsDir+"ABQWE.gpx",true);
-    BOOST_CHECK_EQUAL(route2.maxLatitude(),Position(1.79964,-1.79662));
-
-    // test with wrong input
-    Route route3 = Route(LogFiles::logsDir+"Q.gpx",true);
+BOOST_AUTO_TEST_CASE(expected_max){
+    std::string filename = "standardRoute-N0731234.gpx";
+    // create new GPX data
+    GridWorldRoute gwr = GridWorldRoute("M"); // just one point on defualt earth
+    createGPXlog(gwr, filename);
+    Route route = Route(LogFiles::logsDir+filename,true);
+    //BOOST_AUTO_TEST_CASE(route.maxLatitude(),);
 }
 
-BOOST_AUTO_TEST_CASE(no_data){
-    Route route = Route(LogFiles::logsDir+"Q.gpx", false);
-    BOOST_TEST_MESSAGE(route.maxLatitude());
-}
+//BOOST_AUTO_TEST_CASE(no_data){
+//    std::string filename = "";
+//    GridWorldRoute gwr = GridWorldRoute()
+//    Route route = Route(LogFiles::logsDir+filename, true);
+//    BOOST_TEST_MESSAGE(route.maxLatitude());
+//}
 
 BOOST_AUTO_TEST_CASE(null_position_list)
 {
     // check to see if Null values are handled
-    // Route route = Route();
-    // BOOST_CHECK_THROW(route.maxLatitude(), std::is_null_pointer);
 }
 
 BOOST_AUTO_TEST_CASE(incorrect_data)
 {
-    // check wether 
-
+    // check
 }
 
 
