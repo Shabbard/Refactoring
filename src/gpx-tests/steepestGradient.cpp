bfc28976a3fe1409e1fa00e25e99013a4828d17b
Adam Sheppard <t7.shab@gmail.com>
2019-04-02 14:49:51 +0100

    Changed tests to use the correct macro for comparing floating point variables

--- src/gpx-tests/steepestGradient.cpp
+++ src/gpx-tests/steepestGradient.cpp
@@ -3,7 +3,6 @@
 #include "logs.h"
 #include "route.h"
 #include "gridworld_route.h"
-#include "xmlgenerator.h"
 
 using namespace GPS;
 
@@ -12,7 +11,7 @@ using namespace GPS;
  * Gradient is calculated from the difference in the y values over (divided by) the
  * difference in x values between two points.
  * 
- * The key for finding the steppest gradient is that you must ignore the sign i.e the positive or negative aspect
+ * The key for finding the steepest gradient is that you must ignore the sign i.e the positive or negative aspect
  * of the number, taking only its absolute value. 
 */
 
@@ -26,33 +25,55 @@ std::string createLogs(std::string name, GridWorldRoute route)
     fileOutput.close();
 
     return fileName;
-
 }
 
 BOOST_AUTO_TEST_SUITE( Steepest_Gradient )
 
 const bool isFileName = true;
 
-// You cannot calculate a gradient from just one point, the program should throw an out of range exception
+const double percentageAccuracy = 0.0001;
+
+// You cannot calculate a gradient from just one point, the program should return 0
 BOOST_AUTO_TEST_CASE( Route_with_one_point )
 {
-    GridWorldRoute routeLog = GridWorldRoute("A");
+    Route route = Route(LogFiles::GPXRoutesDir + "Q.gpx", isFileName);
+    BOOST_CHECK_CLOSE( route.steepestGradient(),  0, percentageAccuracy);
+}
 
-    Route route = Route(LogFiles::GPXRoutesDir + createLogs("RouteWithOnePoint", routeLog), isFileName);
-    BOOST_CHECK_EQUAL( route.steepestGradient(),  0);
+// check values with only negative gradients
+
+BOOST_AUTO_TEST_CASE( Route_with_only_negative_gradients )
+{
+    GridWorldRoute routeLog = GridWorldRoute("MHDYV", GridWorld(Earth::NorthPole, 10000, 1000));
+
+    Route route = Route(LogFiles::GPXRoutesDir + createLogs("RouteWithOnlyNegativeGradients", routeLog), isFileName);
+    BOOST_CHECK_CLOSE( route.steepestGradient(),  0, percentageAccuracy);
+}
+
+// check different values with only positive gradients
+
+BOOST_AUTO_TEST_CASE( Route_with_only_positive_gradients )
+{
+    GridWorldRoute routeLog = GridWorldRoute("MQV", GridWorld(Earth::NorthPole, 10000, 1000));
+
+    Route route = Route(LogFiles::GPXRoutesDir + createLogs("RouteWithOnlyPositiveGradients", routeLog), isFileName);
+    BOOST_CHECK_CLOSE( route.steepestGradient(),  0, percentageAccuracy); // use correct macro for floating point numbers
+}
+
+// check values with both positive and negative gradients
+
+BOOST_AUTO_TEST_CASE( Route_with_positive_and_negative_gradients )
+{
+    GridWorldRoute routeLog = GridWorldRoute("MICW", GridWorld(Earth::NorthPole, 10000, 1000));
+
+    Route route = Route(LogFiles::GPXRoutesDir + createLogs("RouteWithPositiveAndNegativeGradients", routeLog), isFileName);
+    BOOST_CHECK_CLOSE( route.steepestGradient(),  0, percentageAccuracy);
 }
 
 // two points on top of each other should have a gradient of 0
 
 // two points with no elevation difference should have a gradient of 0
 
-// check different values with only positive gradients
-
-// check values with only negative gradients
-
-// check values with both positive and negative gradients
-
 // check values with the steepest gradient being at the beginning, middle and end of the list of points
 
- 
 BOOST_AUTO_TEST_SUITE_END()
