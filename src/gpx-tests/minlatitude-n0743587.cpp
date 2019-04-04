4cef0daf2d2e52e7c6db9a7d6cb1589170722665
Finlay Mckinnon <n0743587@my.ntu.ac.uk>
2019-04-04 23:20:36 +0100

    Replaced tests and added log files
    Former-commit-id: 2c1e146ea7d6b77d7487f81f1843d52f884e6913

--- src/gpx-tests/minlatitude-n0743587.cpp
+++ src/gpx-tests/minlatitude-n0743587.cpp
@@ -66,19 +66,19 @@ BOOST_AUTO_TEST_CASE(ZeroLatLon){
 }
 
 
-BOOST_AUTO_TEST_CASE(InvalidFile){
-//Throws exception
-    //Route route = Route(LogFiles::GPXRoutesDir + "invalid.gpx", true);
-    //BOOST_CHECK_THROW( route.minLatitude(), std::domain_error);
+BOOST_AUTO_TEST_CASE(DuplicatePoint){
 
-    //route = Route(LogFiles::GPXRoutesDir + "invalid", true);
-    //BOOST_CHECK_THROW( route.minLatitude(), std::invalid_argument);
+    Route route = Route(LogFiles::GPXRoutesDir + "DuplicatePointA-N0743587.gpx", true);
+    BOOST_CHECK_EQUAL( route.minLatitude(), 0.017996399999999999);
+
+    route = Route(LogFiles::GPXRoutesDir + "DuplicatePointQ-N0743587.gpx", true);
+    BOOST_CHECK_EQUAL( route.minLatitude(), -0.0089981999999999996);
 }
 
-BOOST_AUTO_TEST_CASE(NullCase){
-//Throws IOException
-    //Route route = Route(LogFiles::GPXRoutesDir + "NullPosition-N0743587.gpx", true);
-    //BOOST_CHECK_EQUAL( route.minLatitude(), false);
+BOOST_AUTO_TEST_CASE(PointsMeterApart){
+
+    Route route = Route(LogFiles::GPXRoutesDir + "PointsMeterApart-N0743587.gpx", true);
+    BOOST_CHECK_EQUAL( route.minLatitude(), 0.00017996399999999999 );
 }
 
 
