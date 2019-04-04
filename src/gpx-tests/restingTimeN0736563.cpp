ca166eae8d11d5897c07b3cc417814a4661932ad
Ben <benantonyjackson@gmail.com>
2019-04-04 18:49:59 +0100

    Added a test case

--- src/gpx-tests/restingTimeN0736563.cpp
+++ src/gpx-tests/restingTimeN0736563.cpp
@@ -15,22 +15,32 @@ BOOST_AUTO_TEST_CASE(noRestingTime)
     GPS::Track track(GPS::LogFiles::GPXTracksDir + "A1B3C.gpx", true);
 
     BOOST_CHECK_EQUAL(track.restingTime(), 0);
-
-    BOOST_CHECK_EQUAL(track.restingTime(), 1);
-
 }
 
 BOOST_AUTO_TEST_CASE(stationary)
 {
+    //Tests cases where the track is completly stationary
     GPS::Track stationary(dir + "Stationary.gpx", true);
-
     BOOST_CHECK_EQUAL(stationary.restingTime(), 40);
+}
 
-    GPS::Track test(dir + "ElevationTest.gpx", true);
+BOOST_AUTO_TEST_CASE (singlePointTrack)
+{
+    //If there is not enought arguments in the gpx data to calculate resting time then the function should throw an invalid argument exception
+    GPS::Track singlePointTrack(dir + "SinglePointTrack.gpx", true);
+    BOOST_CHECK_THROW(singlePointTrack.restingTime(), std::invalid_argument);
+}
 
-    std::cout << test.restingTime();
+BOOST_AUTO_TEST_CASE (onlyElevationChanges)
+{
+    //Tests cases where only the elevation is changing
 
-    BOOST_CHECK_EQUAL(stationary.restingTime(), 41);
+    GPS::Track elevationTest1(dir + "ElevationTest1.gpx", true);
+    GPS::Track elevationTest2(dir + "ElevationTest2.gpx", true);
+
+    BOOST_CHECK_EQUAL(elevationTest1.restingTime(), 0);
+    BOOST_CHECK_EQUAL(elevationTest2.restingTime(), 20);
+
 }
 
 BOOST_AUTO_TEST_SUITE_END();
