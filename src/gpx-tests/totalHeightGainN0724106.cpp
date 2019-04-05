98afc695a534dde20d7ba378633363ff2c9bf9ea
Alex Brownlie - N0724106 <n0724106@my.ntu.ac.uk>
2019-04-05 09:15:16 +0100

    Committing my C++ test file, with all of the generated GPX files.
    Former-commit-id: f1e1fd9d011887aec8e269fe36853d570ff67706

--- src/gpx-tests/totalHeightGainN0724106.cpp
+++ src/gpx-tests/totalHeightGainN0724106.cpp
@@ -8,81 +8,76 @@ using namespace GPS;
 
 /*
  * The main complication with this function is that it ignores all the negative differences
- * between successive route points - that is it ignores any decline between two points in its calculation, it only
+ * between successive route points - that is it ignores any decline between two points in its calculation - it only
  * adds together the inclines.
  * 
  * First of all this test suite will check that two specific points return the correct 
  * value. It will also check that if there are two successive points with a decline in 
  * elevation, the function notices this and returns a value of 0 for these two points.
  * 
- * I will also check that 4 successive points return the correct total height gain, with four tests.
- *      All of the points are consecutive inclines in elevation.
- *      All of points are consecutive declines in elevation.
- *      There is a net decline between the points.
- *      There is a net incline between the points.
- * 
- *  For this I will have to generate log files with only two, three or four points.
- * 
- * USING GRIDWORLD
- *  Gridworld allows for curvature of the earth, then you can choose points for either a track or a route
- * 
+ * I will also check that successive points return the correct total height gain, with
+ * four tests.
+ *      Three points that are consecutive inclines in elevation.
+ *      Three points that are consecutive declines in elevation.
+ *      Five points with a net incline between all the points.
+ *      Five points with a net incline between all the points.
  */
 
-
-// metres totalHeightGain() const;
 
 BOOST_AUTO_TEST_SUITE( Route_totalHeightGain )
 const bool isFileName = true;
 
 BOOST_AUTO_TEST_CASE ( twoPositionsIncline ) {
     
-    Route route = Route(LogFiles::GPXRoutesDir + "twoPositionsIncline.gpx", isFileName);
+    Route route = Route(GPS::LogFiles::GPXRoutesDir + "twoPositionsInclineN0724106.gpx", isFileName);
     // Looking at the two points you can see what the height gain should be
-    BOOST_CHECK_EQUAL( route.totalHeightGain(), 5 );
+    BOOST_CHECK_EQUAL( route.totalHeightGain(), 50 );
     
 }
 
 BOOST_AUTO_TEST_CASE ( twoPositionsDecline ) {
     
-    Route route = Route(LogFiles::GPXRoutesDir + "twoPositionsDecline.gpx", isFileName);
+    Route route = Route(GPS::LogFiles::GPXRoutesDir + "twoPositionsDeclineN0724106.gpx", isFileName);
     // If there is a decline between two points, the function should return 0
     BOOST_CHECK_EQUAL( route.totalHeightGain(), 0 );
     
 }
 
-BOOST_AUTO_TEST_CASE ( fourPositionsConsecutiveIncline ) {
+BOOST_AUTO_TEST_CASE ( threePositionsConsecutiveIncline ) {
     
-    Route route = Route(LogFiles::GPXRoutesDir + "fourPositionsConsecutiveIncline.gpx", isFileName);
-    // Looking at the four points you can see what the height gain should be
-    BOOST_CHECK_EQUAL( route.totalHeightGain(), 15 );
+    Route route = Route(GPS::LogFiles::GPXRoutesDir + "threePositionsInclineN0724106.gpx", isFileName);
+    // Looking at the three points you can see what the height gain should be
+    BOOST_CHECK_EQUAL( route.totalHeightGain(), 100 );
     
 }
 
-BOOST_AUTO_TEST_CASE ( fourPositionsConsecutiveDecline ) {
+BOOST_AUTO_TEST_CASE ( threePositionsConsecutiveDecline ) {
     
-    Route route = Route(LogFiles::GPXRoutesDir + "fourPositionsConsecutiveDecline.gpx", isFileName);
-    // As there is four consecutive declines, the total height gain should be 0
+    Route route = Route(GPS::LogFiles::GPXRoutesDir + "threePositionsDeclineN0724106.gpx", isFileName);
+    // As there is three consecutive declines, the total height gain should be 0
     BOOST_CHECK_EQUAL( route.totalHeightGain(), 0 );
     
 }
 
-BOOST_AUTO_TEST_CASE ( fourPositionsNetIncline ) {
+BOOST_AUTO_TEST_CASE ( fivePositionsNetIncline ) {
     
-    Route route = Route(LogFiles::GPXRoutesDir + "fourPositionsNetIncline.gpx", isFileName);
-    // Looking at the four points you can see what the height gain should be
-    BOOST_CHECK_EQUAL( route.totalHeightGain(), 11 );
+    Route route = Route(GPS::LogFiles::GPXRoutesDir + "fivePositionsNetInclineN0724106.gpx", isFileName);
+    // Looking at the five points you can see what the total height gain should be
+    BOOST_CHECK_EQUAL( route.totalHeightGain(), 100 );
     
 }
 
-BOOST_AUTO_TEST_CASE ( fourPositionsNetDecline ) {
+// This test checks that if there are inclines followed by declines,
+// the declines are NOT subtracted from the total height gain.
+BOOST_AUTO_TEST_CASE ( fivePositionsNetDecline ) {
     
-    Route route = Route(LogFiles::GPXRoutesDir + "fourPositionsNetDecline.gpx", isFileName);
+    Route route = Route(GPS::LogFiles::GPXRoutesDir + "fivePositionsNetDeclineN0724106.gpx", isFileName);
     
     /* Looking at the four points you can see what the height gain should be.
      * Even though there is a net decline, the inclines should still be 
      * added to the total height gain. */
     
-    BOOST_CHECK_EQUAL( route.totalHeightGain(), 3 );
+    BOOST_CHECK_EQUAL( route.totalHeightGain(), 50 );
     
 }
 
