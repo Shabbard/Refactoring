617f791f5af966f1802bb0170e143a0ef7c237c2
whoboy2 <thewhoboy2@gmai.com>
2019-04-05 12:37:06 +0000

    Moved repeated code into createLog function Chnaged Horizontal and verticle units to values so that pre-calcualted gradients are more sensible
    Former-commit-id: 10d3036d3933e1df3af88c75661b8e389ab04c95

--- src/gpx-tests/maxGradient-n0756303.cpp
+++ src/gpx-tests/maxGradient-n0756303.cpp
@@ -1,34 +1,39 @@
 #include <boost/test/unit_test.hpp>
 
-#include <fstream>
-#include <string>
-
 #include "earth.h"
+#include "route.h"
 #include "gridworld.h"
 #include "gridworld_route.h"
 #include "logs.h"
-#include "route.h"
+#include "types.h"
+
+
+#include <fstream>
+#include <string>
+
 
 using namespace GPS;
 
 const degrees TOLERANCE = 0.5;
-const metres HORIZONTALGRIDUNIT = 10000;
-const metres VERTICLEGRIDUNIT = 1000;
+const metres HORIZONTALGRIDUNIT = 100;
+const metres VERTICLEGRIDUNIT = 100;
 
 //Gradients were calculated by hand with help from below link + Wolfram Alpha
 //https://math.stackexchange.com/questions/707673/find-angle-in-degrees-from-one-point-to-another-in-2d-space
-const degrees GRADIENT = 5.71059314;
-
+const degrees GRADIENT = 45;
 //Diagonal gradient required the use of pythag to form distance to diagonal points
-const degrees DIAGONALGRADIENT = 4.04469124;
+const degrees DIAGONALGRADIENT = 35.26438968;
 
-//Create or overwrite a log file containing created route
-std::string createLog(std::string logName, GridWorldRoute route) {
+//Create or overwrite a log file containing created route, returns the path of generated log
+std::string createLog(std::string logName, std::string route) {
+	
+	GridWorld grid(Earth::CliftonCampus, HORIZONTALGRIDUNIT, VERTICLEGRIDUNIT);
+	GridWorldRoute gridRoute(route, grid);
 	
 	std::string path = LogFiles::GPXRoutesDir + logName + "_n0756303.gpx";
 	std::ofstream file(path);
 	
-	file << route.toGPX(true, logName);
+	file << gridRoute.toGPX(true, logName);
 	file.close();
 	return path;
 	
@@ -36,13 +41,12 @@ std::string createLog(std::string logName, GridWorldRoute route) {
 
 BOOST_AUTO_TEST_SUITE( Route_maxGradient_n0756303 )
 
+//No test case for 0 points as this is asserted in route.cpp
+
 //Check that any route with one point returns a maxGradient of 0
-BOOST_AUTO_TEST_CASE(onePoint)
-{
-	GridWorld grid(Earth::CliftonCampus);
-	GridWorldRoute gridRoute("M", grid);
-	
-	std::string path = createLog("singlePointRoute", gridRoute);
+BOOST_AUTO_TEST_CASE( onePoint )
+{	
+	std::string path = createLog("singlePointRoute", "M");
 	Route route(path, true);
 	
 	BOOST_CHECK_CLOSE(route.maxGradient(), 0, TOLERANCE);
@@ -51,10 +55,7 @@ BOOST_AUTO_TEST_CASE(onePoint)
 //Check that any route consisiting of only flat point returns a maxGradient of 0
 BOOST_AUTO_TEST_CASE( route_flat )
 {
-	GridWorld grid(Earth::CliftonCampus, HORIZONTALGRIDUNIT, VERTICLEGRIDUNIT);
-	GridWorldRoute gridRoute("ABC", grid);
-	
-	std::string path = createLog("flatRoute", gridRoute);
+	std::string path = createLog("flatRoute", "ABC");
 	Route route(path, true);
 	
 	BOOST_CHECK_CLOSE(route.maxGradient(), 0, TOLERANCE);
@@ -63,10 +64,7 @@ BOOST_AUTO_TEST_CASE( route_flat )
 //Check that a route with all accending points, returns a positive version of a pre calculated angle
 BOOST_AUTO_TEST_CASE( up_route )
 {
-	GridWorld grid(Earth::CliftonCampus, HORIZONTALGRIDUNIT, VERTICLEGRIDUNIT);
-	GridWorldRoute gridRoute("CHM", grid);
-	
-	std::string path = createLog("upRoute", gridRoute);
+	std::string path = createLog("upRoute", "CHM");
 	Route route(path, true);
 	
 	BOOST_CHECK_CLOSE(route.maxGradient(), GRADIENT, TOLERANCE);
@@ -75,10 +73,7 @@ BOOST_AUTO_TEST_CASE( up_route )
 //Check that a route with all decending points, returns a negative version of a pre calculated angle
 BOOST_AUTO_TEST_CASE( down_route )
 {
-	GridWorld grid(Earth::CliftonCampus, HORIZONTALGRIDUNIT, VERTICLEGRIDUNIT);
-	GridWorldRoute gridRoute("MHC", grid);
-	
-	std::string path = createLog("downRoute", gridRoute);
+	std::string path = createLog("downRoute", "MHC");
 	Route route(path, true);
 	
 	BOOST_CHECK_CLOSE(route.maxGradient(), - GRADIENT, TOLERANCE);
@@ -87,10 +82,7 @@ BOOST_AUTO_TEST_CASE( down_route )
 //Check that a diagonal route with all accending points, returns a positive version of pre calculated value
 BOOST_AUTO_TEST_CASE( diagonal_up_route )
 {
-	GridWorld grid(Earth::CliftonCampus, HORIZONTALGRIDUNIT, VERTICLEGRIDUNIT);
-	GridWorldRoute gridRoute("EIM", grid);
-	
-	std::string path = createLog("diagUpRoute", gridRoute);
+	std::string path = createLog("diagUpRoute", "EIM");
 	Route route(path, true);
 	
 	BOOST_CHECK_CLOSE(route.maxGradient(), DIAGONALGRADIENT, TOLERANCE);
@@ -99,10 +91,7 @@ BOOST_AUTO_TEST_CASE( diagonal_up_route )
 //Check that a diagonal route with all decending points, returns a negative version of pre calculated value
 BOOST_AUTO_TEST_CASE( diagonal_down_route )
 {
-	GridWorld grid(Earth::CliftonCampus, HORIZONTALGRIDUNIT, VERTICLEGRIDUNIT);
-	GridWorldRoute gridRoute("MIE", grid);
-	
-	std::string path = createLog("diagDownRoute", gridRoute);
+	std::string path = createLog("diagDownRoute", "MIE");
 	Route route(path, true);
 	
 	BOOST_CHECK_CLOSE(route.maxGradient(), -DIAGONALGRADIENT, TOLERANCE);
@@ -111,10 +100,7 @@ BOOST_AUTO_TEST_CASE( diagonal_down_route )
 //Check a route with an uphill and downhill section returns the gradient of uphill section
 BOOST_AUTO_TEST_CASE( diagonal_up_down_route )
 {
-	GridWorld grid(Earth::CliftonCampus, HORIZONTALGRIDUNIT, VERTICLEGRIDUNIT);
-	GridWorldRoute gridRoute("UQMIE", grid);
-	
-	std::string path = createLog("diagUpDownRoute", gridRoute);
+	std::string path = createLog("diagUpDownRoute", "UQMIE");
 	Route route(path, true);
 	
 	BOOST_CHECK_CLOSE(route.maxGradient(), DIAGONALGRADIENT, TOLERANCE);
@@ -122,10 +108,7 @@ BOOST_AUTO_TEST_CASE( diagonal_up_down_route )
 //Check a route containing a flat section follwoed by an uphill section
 BOOST_AUTO_TEST_CASE( flat_up_route )
 {
-	GridWorld grid(Earth::CliftonCampus, HORIZONTALGRIDUNIT, VERTICLEGRIDUNIT);
-	GridWorldRoute gridRoute("ABCHM", grid);
-	
-	std::string path = createLog("flatUpDownRoute", gridRoute);
+	std::string path = createLog("flatUpDownRoute", "ABCHM");
 	Route route(path, true);
 	
 	BOOST_CHECK_CLOSE(route.maxGradient(), GRADIENT, TOLERANCE);
@@ -134,21 +117,15 @@ BOOST_AUTO_TEST_CASE( flat_up_route )
 //Check a route contaning a downhill section followed by a flat section, max gradient is 0 as downhill is negative
 BOOST_AUTO_TEST_CASE( down_flat_route )
 {
-	GridWorld grid(Earth::CliftonCampus, HORIZONTALGRIDUNIT, VERTICLEGRIDUNIT);
-	GridWorldRoute gridRoute("MHCBA", grid);
-	
-	std::string path = createLog("downFlatRoute", gridRoute);
+	std::string path = createLog("downFlatRoute", "MHCBA");
 	Route route(path, true);
 	
 	BOOST_CHECK_CLOSE(route.maxGradient(), 0, TOLERANCE);
 }
-//Check a route containing a section of diagnonally accending and decending, a flat portion, and then accending again
+//Check a route containing flat, uphill and downhill sections
 BOOST_AUTO_TEST_CASE( diagonal_up_down_flat_up_route )
 {
-	GridWorld grid(Earth::CliftonCampus, HORIZONTALGRIDUNIT, VERTICLEGRIDUNIT);
-	GridWorldRoute gridRoute("UQMIEDCHM", grid);
-	
-	std::string path = createLog("diagUpDownFlatUpRoute", gridRoute);
+	std::string path = createLog("diagUpDownFlatUpRoute", "UQMIEDCHM");
 	Route route(path, true);
 	
 	BOOST_CHECK_CLOSE(route.maxGradient(), GRADIENT, TOLERANCE);
