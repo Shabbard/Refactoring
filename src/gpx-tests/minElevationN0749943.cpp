505502e4a3f9f516ff154d7eb99a9e7639777392
Ryan Campbell <n0749943@my.ntu.ac.uk>
2019-04-05 14:15:53 +0100

    Created more tests in test file. Associated logs with tests.

--- src/gpx-tests/minElevationN0749943.cpp
+++ src/gpx-tests/minElevationN0749943.cpp
@@ -10,8 +10,8 @@ BOOST_AUTO_TEST_SUITE(minElevationN0749943)
 
 const bool isFileName = true;
 
-//Ensure that minElevation returns the correct result for a regular route.
-BOOST_AUTO_TEST_CASE( RegularRoute )
+//Ensure that minElevation returns the correct result for typical valid routes.
+BOOST_AUTO_TEST_CASE( TypicalRoute )
 {
     Route testRoute = Route(LogFiles::GPXRoutesDir + "NorthYorkMoors.gpx", isFileName);
     BOOST_CHECK_EQUAL(testRoute.minElevation(), 135);
@@ -21,6 +21,13 @@ BOOST_AUTO_TEST_CASE( RegularRoute )
 BOOST_AUTO_TEST_CASE( SingleValue )
 {
    Route testRoute = Route(LogFiles::GPXRoutesDir + "SingleValueN0749943.gpx", isFileName);
+   BOOST_CHECK_EQUAL(testRoute.minElevation(), 5);
+}
+
+//Ensure that minElevation returns the correct result with only two positions.
+BOOST_AUTO_TEST_CASE( TwoValues )
+{
+   Route testRoute = Route(LogFiles::GPXRoutesDir + "TwoValuesN0749943.gpx", isFileName);
    BOOST_CHECK_EQUAL(testRoute.minElevation(), 5);
 }
 
@@ -36,6 +43,34 @@ BOOST_AUTO_TEST_CASE( NegativeElevation )
 {
     Route testRoute = Route(LogFiles::GPXRoutesDir + "NegativeElevationN0749943.gpx", isFileName);
     BOOST_CHECK_EQUAL(testRoute.minElevation(), -3);
+}
+
+//Ensure that minElevation returns the correct value where elevation values differ by hundreds of units.
+BOOST_AUTO_TEST_CASE( HundredDifference )
+{
+    Route testRoute = Route(LogFiles::GPXRoutesDir + "HundredDifferenceN0749943.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testRoute.minElevation(), 100);
+}
+
+//Ensure that minElevation returns the correct value where elevation values differ by thousands of units.
+BOOST_AUTO_TEST_CASE( ThousandDifference )
+{
+    Route testRoute = Route(LogFiles::GPXRoutesDir + "ThousandDifferenceN0749943.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testRoute.minElevation(), 3421);
+}
+
+//Ensure that minElevation returns the correct value where elvations values differ by tenths of a unit.
+BOOST_AUTO_TEST_CASE( OneDecimalPlaceDifference )
+{
+    Route testRoute = Route(LogFiles::GPXRoutesDir + "OneDecimalDifferenceN0749943.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testRoute.minElevation(), 0.1);
+}
+
+//Ensure that minElevation returns the correct value where elvations values differ by hundreths of a unit.
+BOOST_AUTO_TEST_CASE( TwoDecimalPlaceDifference )
+{
+    Route testRoute = Route(LogFiles::GPXRoutesDir + "TwoDecimalDifferenceN0749943.gpx", isFileName);
+    BOOST_CHECK_EQUAL(testRoute.minElevation(), 0.18);
 }
 
 //Ensure that the minElevation is 0 when all positions have an elevation of 0.
