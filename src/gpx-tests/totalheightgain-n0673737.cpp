9c70236ff76735046c4beba4e13d3fdeb86fe418
Hannah Schofield <n0673737@my.ntu.ac.uk>
2019-04-12 14:38:54 +0100

    All final files. GPX-Test, loggeneration-n0673737, and totalheightgain-n0673737
    Former-commit-id: 79819efdca29cfef71f70744da388f1a92d9aa9f

--- src/gpx-tests/totalheightgain-n0673737.cpp
+++ src/gpx-tests/totalheightgain-n0673737.cpp
@@ -13,7 +13,30 @@ const bool isFileName = true;
 //When there is a decrease in height, there is no total height gain aka 0
 BOOST_AUTO_TEST_CASE(DecreasebetweenPoints){
     Route route = Route(LogFiles::GPXRoutesDir + "DecreasebetweenPoints-n0673737.gpx" isFileName);
-    BOOST_CHECK_EQUAL(route.totalHeightGain(), 0);
+    BOOST_CHECK_EQUAL(route.totalHeightGain(), 0);//0 height gain
 }
 
+//When there is an increase in height, the total height is recorded
+BOOST_AUTO_TEST_CASE(IncreasebetweenPoints){
+    Route route = Route(LogFiles::GPXRouteDir + "IncreasebetweenPoints-n0673737.gpx" isFileName);
+    BOOST_CHECK_EQUAL(route.totalHeightGain(), 261);//is the elevation in metres of the final point
+}
+
+//When points are repeated, the elevation still calculates correctly
+BOOST_AUTO_TEST_CASE(RepeatedPoints){
+    Route route = Route(LogFiles::GPXRouteDir + "RepeatedPoints-n0673737.gpx" isFileName);
+    BOOST_CHECK_EQUAL(route.totalHeightGain(),1956); //Total elevation in metres between the points if the journey was done twice
+}
+
+//Three points results in large increase. The function is then shown not to be limited
+BOOST_AUTO_TEST_CASE(LargeIncrease){
+    Route route = Route(LogFiles::GPXRouteDir + "LargeIncrease-n0673737.gps" isFileName);
+    BOOST_CHECK_EQUAL(route.totalHeightGain(),8848)
+}
+
+//Three points results in small increase. The function is shown to not be limited
+BOOST_AUTO_TEST_CASE(SmallIncrease){
+    Route route = Route(LogFiles::GPXRouteDir + "SmallIncrease-n0673737.gpx" isFileName);
+    BOOST_CHECK_EQUAL(route.totalHeightGain(),58)
+}
 BOOST_AUTO_TEST_END()
