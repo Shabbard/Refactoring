a62f9878990078b84a50a0ace943c037b86852bd
Hannah Schofield <n0673737@my.ntu.ac.uk>
2019-04-12 13:54:36 +0100

    Tests file
    Former-commit-id: 791ca303db61fdd956030218679081cfa0c2ae85

--- src/gpx-tests/totalheightgain-n0673737.cpp
+++ src/gpx-tests/totalheightgain-n0673737.cpp
@@ -1,15 +1,19 @@
-#inlcude <boost/test/unit_test.hpp>
-#inlcude "logs/h"
-#inlcude "types.h"
+#include <boost/test/unit_test.hpp>
+
+#include "logs/h"
+#include "types.h"
 #include "route.h"
 
 using namespace GPS;
 
+//File name
 BOOST_AUTO_TEST_SUITE(totalHeightGain_n0673737)
 const bool isFileName = true;
 
-BOOST_AUTO_TEST_CASE (){
-
+//When there is a decrease in height, there is no total height gain aka 0
+BOOST_AUTO_TEST_CASE(DecreasebetweenPoints){
+    Route route = Route(LogFiles::GPXRoutesDir + "DecreasebetweenPoints-n0673737.gpx" isFileName);
+    BOOST_CHECK_EQUAL(route.totalHeightGain(), 0);
 }
 
 BOOST_AUTO_TEST_END()
