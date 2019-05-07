a349b0c83e54e1a3c07d9521817a87ec308a91a0
Adam Sheppard <t7.shab@gmail.com>
2019-05-07 15:06:58 +0100

    Refactored a little

--- src/track.cpp
+++ src/track.cpp
@@ -92,45 +92,124 @@ speed Track::maxRateOfDescent() const
 
 Track::Track(std::string source, bool isFileName, metres granularity)
 {
+
+
+    ////////////////////////////////////////////////////////////////
+    //                checkAndGetElementContent()                 //
+    // Function to check that each element is in the file         //
+    ////////////////////////////////////////////////////////////////
+
+    auto checkAndGetElementContent = [](std::string source, std::string type)
+    {
+        if (!XML::Parser::elementExists(source, type))  { throw std::domain_error("No " + type + " element."); }
+        return XML::Parser::getElementContent(XML::Parser::getElement(source, type));        
+    };
+
+    ////////////////////////////////////////////////////////
+    //           checkAndGetElementAttribute()            //
+    // Function to check that each element is in the file //
+    ////////////////////////////////////////////////////////
+
+    auto checkAndGetElementAttribute = [](std::string source, std::string type)
+    {
+        if (!XML::Parser::attributeExists(source, type)) { throw std::domain_error("No " + type + " attribute."); }
+        return XML::Parser::getElementAttribute(source, type); 
+    };
+
+    ///////////////////////////////////////////////////////
+    //                  runThroughFile()                 //
+    // Function to get the stream of strings from a file //
+    ///////////////////////////////////////////////////////
+
+    auto runThroughFile = [](std::string source, bool isFileName)
+    {
+        std::ostringstream ossPrivate, ossToReturn;
+        if (isFileName)
+        {
+            std::ifstream fs(source);
+            if (!fs.good())
+                throw std::invalid_argument("Error opening source file '" + source + "'.");
+            ossPrivate << "Source file '" << source << "' opened okay." << std::endl;
+            while (fs.good())
+            {
+                std::string fileLine;
+                getline(fs, fileLine);
+                ossToReturn << fileLine << std::endl;
+            }
+            //source = oss2.str();     
+            return ossToReturn.str(); 
+        }
+        return source;
+    };
+
+    auto checkAndEraseElement = [](std::string source, std::string type)
+    {
+        return XML::Parser::getAndEraseElement(source, type);
+    };
+
+    ////////////////////////
+    // Constant variables //
+    ////////////////////////
+
+    const std::string GPXSTRING = "gpx";
+    const std::string RTKSTRING = "trk";
+    const std::string TRKSEGSTRING = "trkseg";
+    const std::string TRKPTSTIRNG = "trkpt";
+    const std::string NAMESTRING = "name";
+    const std::string LATSTRING = "lat";
+    const std::string LONSTRING = "lon";
+    const std::string ELESTRING = "ele";
+
+    //////////////////////////////////////////
+    // Variables that may change throughout //
+    //////////////////////////////////////////
+
     std::string mergedTrkSegs,trkseg,lat,lon,ele,name,time,temp,temp2;
     metres deltaH,deltaV;
     seconds startTime, currentTime, timeElapsed;
-    std::ostringstream oss,oss2;
-    unsigned int num;
-    this->granularity = granularity;
-    if (isFileName) {
-        std::ifstream fs(source);
-        if (! fs.good()) throw std::invalid_argument("Error opening source file '" + source + "'.");
-        oss << "Source file '" << source << "' opened okay." << std::endl;
-        while (fs.good()) {
-            getline(fs, temp);
-            oss2 << temp << std::endl;
-        }
-        source = oss2.str();
+    std::ostringstream stringStream;
+    unsigned int numOfPosition;
+    setGranularity(granularity);
+
+
+
+
+
+
+    //////////////////////////////////////////////////////////////////////////
+    // Uses Lambda to find source for each stringType & opening source file //
+    //////////////////////////////////////////////////////////////////////////
+
+    source = runThroughFile(source, isFileName);
+    source = checkAndGetElementContent(source, GPXSTRING);
+    source = checkAndGetElementContent(source, RTKSTRING);
+
+
+    if (XML::Parser::elementExists(source, NAMESTRING)) {
+        routeName = checkAndGetElementContent(checkAndEraseElement(source,NAMESTRING), NAMESTRING);
+        stringStream << "Track name is: " << routeName << std::endl;
     }
-    if (! XML::Parser::elementExists(source,"gpx")) throw std::domain_error("No 'gpx' element.");
-    temp = XML::Parser::getElement(source, "gpx");
-    source = XML::Parser::getElementContent(temp);
-    if (! XML::Parser::elementExists(source,"trk")) throw std::domain_error("No 'trk' element.");
-    temp = XML::Parser::getElement(source, "trk");
-    source = XML::Parser::getElementContent(temp);
-    if (XML::Parser::elementExists(source, "name")) {
-        temp = XML::Parser::getAndEraseElement(source, "name");
-        routeName = XML::Parser::getElementContent(temp);
-        oss << "Track name is: " << routeName << std::endl;
-    }
+
     while (XML::Parser::elementExists(source, "trkseg")) {
-        temp = XML::Parser::getAndEraseElement(source, "trkseg");
-        trkseg = XML::Parser::getElementContent(temp);
+        
+        trkseg = XML::Parser::getElementContent(XML::Parser::getAndEraseElement(source, "trkseg"));
         XML::Parser::getAndEraseElement(trkseg, "name");
         mergedTrkSegs += trkseg;
     }
+
+
+
     if (! mergedTrkSegs.empty()) source = mergedTrkSegs;
-    num = 0;
+    numOfPosition = 0;
+
+    
     if (! XML::Parser::elementExists(source,"trkpt")) throw std::domain_error("No 'trkpt' element.");
     temp = XML::Parser::getAndEraseElement(source, "trkpt");
     if (! XML::Parser::attributeExists(temp,"lat")) throw std::domain_error("No 'lat' attribute.");
     if (! XML::Parser::attributeExists(temp,"lon")) throw std::domain_error("No 'lon' attribute.");
+
+
+    lat = checkAndGetElementAttribute(temp,LATSTRING);
     lat = XML::Parser::getElementAttribute(temp, "lat");
     lon = XML::Parser::getElementAttribute(temp, "lon");
     temp = XML::Parser::getElementContent(temp);
@@ -139,26 +218,30 @@ Track::Track(std::string source, bool isFileName, metres granularity)
         ele = XML::Parser::getElementContent(temp2);
         Position startPos = Position(lat,lon,ele);
         positions.push_back(startPos);
-        oss << "Start position added: " << startPos.toString() << std::endl;
-        ++num;
+        stringStream << "Start position added: " << startPos.toString() << std::endl;
+        ++numOfPosition;
     } else {
         Position startPos = Position(lat,lon);
         positions.push_back(startPos);
-        oss << "Start position added: " << startPos.toString() << std::endl;
-        ++num;
+        stringStream << "Start position added: " << startPos.toString() << std::endl;
+        ++numOfPosition;
     }
+
     if (XML::Parser::elementExists(temp,"name")) {
         temp2 = XML::Parser::getElement(temp,"name");
         name = XML::Parser::getElementContent(temp2);
     }
+
     positionNames.push_back(name);
     arrived.push_back(0);
     departed.push_back(0);
     if (! XML::Parser::elementExists(temp,"time")) throw std::domain_error("No 'time' element.");
+
     temp2 = XML::Parser::getElement(temp,"time");
     time = XML::Parser::getElementContent(temp2);
     startTime = currentTime = stringToTime(time);
     Position prevPos = positions.back(), nextPos = positions.back();
+    
     while (XML::Parser::elementExists(source, "trkpt")) {
         temp = XML::Parser::getAndEraseElement(source, "trkpt");
         if (! XML::Parser::attributeExists(temp,"lat")) throw std::domain_error("No 'lat' attribute.");
@@ -178,7 +261,7 @@ Track::Track(std::string source, bool isFileName, metres granularity)
         if (areSameLocation(nextPos, prevPos)) {
             // If we're still at the same location, then we haven't departed yet.
             departed.back() = currentTime - startTime;
-            oss << "Position ignored: " << nextPos.toString() << std::endl;
+            stringStream << "Position ignored: " << nextPos.toString() << std::endl;
         } else {
             if (XML::Parser::elementExists(temp,"name")) {
                 temp2 = XML::Parser::getElement(temp,"name");
@@ -189,25 +272,26 @@ Track::Track(std::string source, bool isFileName, metres granularity)
             timeElapsed = currentTime - startTime;
             arrived.push_back(timeElapsed);
             departed.push_back(timeElapsed);
-            oss << "Position added: " << nextPos.toString() << std::endl;
-            oss << " at time: " << std::to_string(timeElapsed) << std::endl;
-            ++num;
+            stringStream << "Position added: " << nextPos.toString() << std::endl;
+            stringStream << " at time: " << std::to_string(timeElapsed) << std::endl;
+            ++numOfPosition;
             prevPos = nextPos;
         }
     }
-    oss << num << " positions added." << std::endl;
+    stringStream << numOfPosition << " positions added." << std::endl;
     routeLength = 0;
-    for (unsigned int i = 1; i < num; ++i ) {
+    for (unsigned int i = 1; i < numOfPosition; ++i ) {
         deltaH = Position::distanceBetween(positions[i-1], positions[i]);
         deltaV = positions[i-1].elevation() - positions[i].elevation();
         routeLength += sqrt(pow(deltaH,2) + pow(deltaV,2));
     }
-    report = oss.str();
+    report = stringStream.str();
 }
 
 void Track::setGranularity(metres granularity)
 {
-    bool implemented = false;
+    bool implemented = true;
+    this->granularity = granularity;
     assert(implemented);
 }
 
